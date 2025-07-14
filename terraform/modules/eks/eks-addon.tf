08f70f9d79a3aaa9f96fc444948ea0d3ac6af857
unknown <ryuinhye95@gmail.com>
2025-07-14 17:48:31 +0900

    2025.07.14 add opensource add-on

--- terraform/modules/eks/eks-addon.tf
+++ terraform/modules/eks/eks-addon.tf
@@ -1,30 +1,30 @@
 # ★ aws-load-balancer-controller 미사용으로 주석 처리(aws_lb_controller_pod_identity도 주석)
-# resource "helm_release" "aws-load-balancer-controller" {
-#   repository = "https://aws.github.io/eks-charts"  
-#   chart      = "aws-load-balancer-controller"  // Helm 차트 패키지 이름  
-#   name       = "aws-load-balancer-controller"  // EKS 배포 시 설정할 차트 이름
-#   namespace  = "kube-system"
-#   version    = var.eks_addon_versions["aws-lb-controller"]
-#   timeout    = 10000 // 15분 토큰 만료 해결 test
+resource "helm_release" "aws-load-balancer-controller" {
+  repository = "https://aws.github.io/eks-charts"  
+  chart      = "aws-load-balancer-controller"  // Helm 차트 패키지 이름  
+  name       = "aws-load-balancer-controller"  // EKS 배포 시 설정할 차트 이름
+  namespace  = "kube-system"
+  version    = var.eks_addon_versions["aws-lb-controller"]
+  timeout    = 10000 // 15분 토큰 만료 해결 test
 
-#   dynamic "set" {
-#     for_each = {
-#       "clusterName"             = module.eks.cluster_name
-#       "serviceAccount.create"   = "true"
-#       "serviceAccount.name"     = "aws-load-balancer-controller-sa"
-#       "nodeSelector.node-group" = "mgmt"
-#       // 아래 옵션은 사용하지 않지만 기본값이 true임, 불필요한 로깅 방지를 위해 비활성화
-#       "enableShield"            = "false"
-#       "enableWaf"               = "false"
-#       "enableWafv2"             = "false"
-#     }
-#     content {
-#       name =  set.key
-#       value = set.value
-#     }
-#   }
-#   depends_on = [module.aws_lb_controller_pod_identity]  
-# }
+  dynamic "set" {
+    for_each = {
+      "clusterName"             = module.eks.cluster_name
+      "serviceAccount.create"   = "true"
+      "serviceAccount.name"     = "aws-load-balancer-controller-sa"
+      "nodeSelector.node-group" = "mgmt"
+      // 아래 옵션은 사용하지 않지만 기본값이 true임, 불필요한 로깅 방지를 위해 비활성화
+      "enableShield"            = "false"
+      "enableWaf"               = "false"
+      "enableWafv2"             = "false"
+    }
+    content {
+      name =  set.key
+      value = set.value
+    }
+  }
+  depends_on = [module.aws_lb_controller_pod_identity]  
+}
 
 
 resource "helm_release" "cluster-autoscaler" {
@@ -68,6 +68,15 @@ resource "helm_release" "cluster-autoscaler" {
   depends_on = [module.cluster_autoscaler_pod_identity]
 }
 
+# ----------------------------------------------------------------------------------
+# helm-release istio 
+# istio-helm chart: https://github.com/istio/istio/tree/master/manifests/charts 
+# terraform helm provider: https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release 
+# 
+# repo check 
+# helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm search repo istio
+# ----------------------------------------------------------------------------------
 # istio-system namespace 사전 생성 
 resource "kubernetes_namespace" "istio_system" {
   metadata {
@@ -75,77 +84,106 @@ resource "kubernetes_namespace" "istio_system" {
   }
 }
 
-# ★ 기존 istio helm 차트 방식 주석 처리
-# resource "helm_release" "istio_base" {
-#   name       = "istio-base"
-#   namespace  = kubernetes_namespace.istio_system.metadata.0.name
-#   repository = "https://istio-release.storage.googleapis.com/charts"
-#   chart      = "base"
-#   version    = "1.24.2"
-#   timeout    = 10000
+# 1. istio-base (CRDs 포함)
+resource "helm_release" "istio_base" {
+  name       = "istio-base"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+  version    = "1.24.2"
+  timeout    = 10000
 
-#   depends_on = [kubernetes_namespace.istio_system]
-# }
+  cleanup_on_fail = true
 
-# resource "helm_release" "istiod" {
-#   name       = "istiod"
-#   namespace  = kubernetes_namespace.istio_system.metadata.0.name
-#   repository = "https://istio-release.storage.googleapis.com/charts"
-#   chart      = "istiod"
-#   version    = "1.24.2"
-#   timeout    = 10000
-#   depends_on = [helm_release.istio_base]
+  depends_on = [kubernetes_namespace.istio_system]
+}
 
-#   set {
-#     name  = "pilot.nodeSelector.node-group"
-#     value = "mgmt"
-#   }
-# }
-
-# resource "helm_release" "istio_ingress" {
-#   name       = "istio-ingress"
-#   namespace  = kubernetes_namespace.istio_system.metadata.0.name
-#   repository = "https://istio-release.storage.googleapis.com/charts"
-#   chart      = "gateway"
-#   version    = "1.24.2"
-#   timeout    = 10000
-#   depends_on = [helm_release.istiod]
-
-#   set {
-#     name  = "service.type"
-#     value = "NodePort"
-#   }
-
-#   set {
-#     name  = "nodeSelector.node-group"
-#     value = "mgmt"
-#   }
-# }
-
-# istioctl install과 동일한 설정으로 istiod 설치
-resource "helm_release" "istio" {
+# 2. istiod (control plane)
+resource "helm_release" "istiod" {
   name       = "istiod"
-  namespace  = kubernetes_namespace.istio_system.metadata.0.name
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "istiod"
   version    = "1.24.2"
   timeout    = 10000
+
   cleanup_on_fail = true
 
-  dynamic "set" {
-    for_each = {
-      "global.proxy.image"                                = "proxyv2"
-      "global.hub"                                        = "docker.inner-thehandsome.com/istio"
-      "gateways.istio-ingressgateway.enabled"             = "true"
-      "gateways.istio-ingressgateway.type"                = "NodePort"
-      "gateways.istio-ingressgateway.nodeSelector.node-group" = "mgmt"
-      "pilot.nodeSelector.node-group"                     = "mgmt"
-    }
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
+  depends_on = [helm_release.istio_base]
 
-  depends_on = [kubernetes_namespace.istio_system]
+  values = [
+    file("${path.module}/helm_values/istio/istiod-values.yaml")
+  ]
+}
+
+# 3. (선택) istio-ingressgateway
+resource "helm_release" "istio_ingress" {
+  name       = "istio-ingressgateway"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "gateway"
+  version    = "1.24.2"
+  timeout    = 10000
+
+  cleanup_on_fail = true
+
+  depends_on = [helm_release.istiod]
+
+  values = [
+    file("${path.module}/helm_values/istio/istio-ingressgateway-values.yaml")
+  ]
+}
+
+# ----------------------------------------------------------------------------------
+# helm release argocd 
+# argcod app version 2.14.2 -> chart version 6.7.4. app version 별도 명시 필요 없음
+# argocd helm chart github: https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd
+# ----------------------------------------------------------------------------------
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "6.7.4" # <- ArgoCD 앱 버전 2.14.2 포함
+
+  create_namespace = false
+  wait             = true
+  timeout          = 300
+
+  values = [
+    file("${path.module}/helm_values/argocd/argocd-values.yaml")
+  ]
+}
+
+
+# ----------------------------------------------------------------------------------
+# helm release fluentd
+# fluentd 1.0.0 install -> helm chart 이용
+# ----------------------------------------------------------------------------------
+resource "kubernetes_namespace" "fluentd" {
+  metadata {
+    name = "fluentd"
+  }
+}
+
+resource "helm_release" "fluentd" {
+  name       = "fluentd"
+  namespace  = kubernetes_namespace.fluentd.metadata[0].name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "${path.module}/helm_values/fluentd"
+  version    = "1.0.0" # chart version
+
+  create_namespace = false
+  wait             = true
+  timeout          = 300
+
+  values = [
+    file("${path.module}/helm_values/fluentd/fluentd-values.yaml")
+  ]
 }
--- terraform/modules/eks/provider.tf
+++ terraform/modules/eks/provider.tf
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------
 # Provider 설정 
 # Terraform에서 kubernetes, helm 접근 할 수 있도록 인증 정보 제공
+# Kubernetes = 2.27.0 (client-go to v0.31.0)  eks 1.31 버전과 호환: https://github.com/hashicorp/terraform-provider-kubernetes/releases/tag/v2.27.0
 # ------------------------------------------------------------------------
 terraform {
   required_version = ">= 1.0"
@@ -9,7 +10,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes" 
+      #version = "~> 2.16.1"
+      version = "= 2.27.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.1"
+    }
   }
+  
 }
 
 provider "aws" {
@@ -19,19 +30,16 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-#  cluster_ca_certificate = base64decode(module.eks.certificate_authority_data)
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-   token                  = data.aws_eks_cluster_auth.eks-blue-auth.token
+  token                  = data.aws_eks_cluster_auth.eks-blue-auth.token
 }
 
+#Helm provider v2.0+에서는 kubernetes 블록 방식이 아닌 직접 속성 설정 방식 사용
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#    cluster_ca_certificate = base64decode(module.eks.certificate_authority[0].data)
-#    token                  = data.aws_eks_cluster_auth.eks-blue-auth.token
-  
-    exec { # 테라폼 실행환경에 awscli 설치 및 설정되어 있는 경우 token 대신 사용 
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--region", "${var.region}",  "--cluster-name", module.eks.cluster_name]
       command     = "aws"
